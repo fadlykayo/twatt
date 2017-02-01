21e6e71af9d5e542820f282b1f0c0c346ef530a2
Fadly Kayo <f_kayo@yahoo.com>
2017-02-01 12:18:20 +0700

    Fixing end all points

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,19 @@ var router = express.Router()
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index')
+  res.send('index')
 })
+
+router.get('/api', function (req, res, next) {
+  res.send('api')
+})
+
+router.get('/api/twitter', function (req, res, next) {
+  res.send('twitter')
+})
+
+// router.get('/api/twitter/search', function (req, res, next) {
+//   res.send('search')
+// })
 
 module.exports = router
